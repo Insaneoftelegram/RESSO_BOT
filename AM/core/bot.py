bac8b1ec5ce80463b9b701f16a1aaac4bbd4cb7b
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-06-27 13:11:55 +0530

    Update bot.py

--- AM/core/bot.py
+++ AM/core/bot.py
@@ -10,7 +10,7 @@ from ..logging import LOGGER
 
 class AnonXBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Starting Resso Bot...")
         super().__init__(
             "AMMusic",
             api_id=config.API_ID,
